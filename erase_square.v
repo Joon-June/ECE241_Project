4fa62e8a4b433ee856d1edee8c23ae3d8047d6ab
Joon <evenn33111@gmail.com>
2018-11-26 09:53:12 -0500

    changes

--- erase_square.v
+++ erase_square.v
@@ -4,12 +4,16 @@ module erase_square(
 	input enable,
     input [3:0]COUNTER_X, //Uppercase indicates grid coutner
     input [3:0]COUNTER_Y, //Uppercase indicates grid coutner
+	 input [8:0]colour_erase_square,
     output [8:0]colour,
 	 output reg erase_square_done,
     output [7:0]x, //Will go into VGA Input
-    output [6:0]y //Will go into VGA Input
+    output [6:0]y, //Will go into VGA Input
+	 output [14:0]map_address
     );
     
+
+	 
     wire [14:0]mem_add;
 	
     reg [4:0]temp_x;
@@ -17,7 +21,7 @@ module erase_square(
 	 reg [4:0]counter_x;
     reg [4:0]counter_y;
 	 
-	 reg [1:0]delay;
+	 reg [2:0]delay;
 
     initial begin
         counter_x = 5'b0;
@@ -26,12 +30,18 @@ module erase_square(
 		  delay = 0;
     end
 	
+	//Connection to current state ram
+	assign colour = colour_erase_square;
+	assign map_address = mem_add;
 	
 	wire [7:0]x_temp_mem;
 	wire [6:0]y_temp_mem;
 	
 	assign x_temp_mem = 8'b0 + (COUNTER_X * 5'b10100) + counter_x;
 	assign y_temp_mem = 7'b0 + (COUNTER_Y * 5'b10100) + counter_y;
+
+	assign x = 8'b0 + COUNTER_X * 5'b10100 + temp_x;
+    assign y = 7'b0 + COUNTER_Y * 5'b10100 + temp_y;
 	
     //Memory Address Translator for 20x20 .mif files
     memory_address_translator_160x120 v1(
@@ -40,29 +50,30 @@ module erase_square(
 									  .mem_address(mem_add) //Connection A
 									  ); 
 
-    //.mif-initialized ram with map background
-	ram19200x9_map_background map_background(
-						.address(mem_add),
-						.clock(clk),
-						.data(9'b0),
-						.wren(1'b0),
-						.q(colour)
-						);
+//    //.mif-initialized ram with map background
+//	ram19200x9_map_background map_background(
+//						.address(mem_add),
+//						.clock(clk),
+//						.data(9'b0),
+//						.wren(1'b0),
+//						.q(colour)
+//						);
 
     always @(posedge clk) begin
 		  if(!resetn) begin
             counter_x <= 0;
             counter_y <= 0;
+			erase_square_done <= 0;
 				temp_x <= 0;
 				temp_y <= 0;
 				delay <= 0;
         end
         else begin
-			if(enable) begin
-				if(counter_x == 0)
-					erase_square_done <= 0;
+			if(counter_x == 0)
+				erase_square_done <= 0;
 
-				if(delay == 2'b11) begin
+			if(enable) begin
+				if(delay == 3'b111) begin
 					temp_x <= counter_x;
 					temp_y <= counter_y;
 				  
@@ -85,7 +96,4 @@ module erase_square(
 			end
         end
     end
-
-    assign x = 8'b0 + COUNTER_X * 5'b10100 + temp_x;
-    assign y = 7'b0 + COUNTER_Y * 5'b10100 + temp_y;
 endmodule
